0e7e1e322883788deb442c8acbaf96f56b4d81d6
xuetao <gentle.taoxue@gmail.com>
2019-09-22 16:35:09 +0800

    add client server connect

---
+++ nettyclient/src/main/java/com/practice/NettyClient.java
@@ -0,0 +1,83 @@
+package com.practice;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+/**
+ * @Author xuetao
+ * @Description: netty 客户端
+ * @Date 2019-09-22
+ * @Version 1.0
+ */
+public class NettyClient {
+
+
+    /**
+     * ip 地址
+     */
+    private final String host;
+
+    /**
+     * 服务端口
+     */
+    private final int port;
+
+    public NettyClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+
+            bootstrap.group(group).channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            socketChannel.pipeline()
+                                    .addLast(new SimpleChannelInboundHandler<ByteBuf>() {
+
+                                        @Override
+                                        public void channelActive(ChannelHandlerContext ctx) {
+                                            ctx.writeAndFlush(Unpooled.copiedBuffer("Hello Server", CharsetUtil.UTF_8));
+                                        }
+
+                                        @Override
+                                        protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) {
+                                            System.out.println("Client received: " + byteBuf.toString(CharsetUtil.UTF_8));
+                                        }
+
+                                        @Override
+                                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                            cause.printStackTrace();
+                                            ctx.close();
+                                        }
+                                    });
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect().sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        new NettyClient("127.0.0.1", 8080).start();
+    }
+}
---
+++ nettyserver/src/main/java/com/practice/NettyServer.java
@@ -0,0 +1,79 @@
+package com.practice;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
+
+import java.net.InetSocketAddress;
+import java.nio.Buffer;
+import java.nio.charset.Charset;
+
+/**
+ * @Author xuetao
+ * @Description: netty服务端
+ * @Date 2019-09-22
+ * @Version 1.0
+ */
+public class NettyServer {
+
+    private final int port;
+
+    public NettyServer(int port) {
+        this.port = port;
+    }
+
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+
+            bootstrap.group(group).channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            socketChannel.pipeline()
+                                    .addLast(new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                            ByteBuf byteBuf = (ByteBuf) msg;
+                                            System.out.println("Server received: " + byteBuf.toString(CharsetUtil.UTF_8));
+                                            String o = "Hello Client";
+                                            byteBuf.clear();
+                                            ctx.write(byteBuf.writeBytes(o.getBytes()));
+                                        }
+
+                                        @Override
+                                        public void channelReadComplete(ChannelHandlerContext ctx) {
+                                            ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+                                        }
+
+                                        @Override
+                                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                            cause.printStackTrace();
+                                            ctx.close();
+                                        }
+                                    });
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.bind().sync();
+            channelFuture.channel().closeFuture().sync();
+        } catch (Exception e) {
+
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new NettyServer(8080).start();
+    }
+}
